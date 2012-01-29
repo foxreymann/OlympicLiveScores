a2ec33f12c34c70b7efdc4fa2ea7a320a4a78c79
tomaszjureczko <tomasz.jureczko@gmail.com>
2012-01-29 04:14:12 +0000

    Main screen ready - up all night coding so sorry for the quality

--- assets/www/a.js
+++ assets/www/a.js
@@ -1,5 +1,15 @@
 console.log('a.js');
 
+String.prototype.supplant = function (o) { 
+    return this.replace(/{([^{}]*)}/g, 
+        function (a, b) {  
+            var r = o[b];
+            return typeof r === 'string' ? 
+                r : a; 
+        }
+    ); 
+};
+
 document.addEventListener("deviceready", function () {
     console.log('deviceready')
     start();
